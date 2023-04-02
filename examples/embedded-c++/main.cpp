3054c421660cd3c5231ad8223a89936889a39f4f
yuliangyong <yuliangyong8295019@gmail.com>
2023-04-02 17:33:30 +0200

    add projection prune

--- examples/embedded-c++/main.cpp
+++ examples/embedded-c++/main.cpp
@@ -11,10 +11,10 @@ int main() {
 //	auto result = con.Query("SELECT * FROM integers");
 //	auto result = con.Query("select count(*) from '/home/liyu/duckdb/data/csv/customer.csv'");
 //	auto result = con.Query("select name from '/home/liyu/duckdb/data/parquet-testing/candidate.parquet' limit 2");
-//	auto result = con.Query("select * from '/home/liyu/duckdb/data/parquet-testing/decimals.parquet'");
+//	auto result = con.Query("select l3 from '/home/liyu/duckdb/data/parquet-testing/decimals.parquet'");
 //	auto result = con.Query("select * from '/home/liyu/duckdb/data/json/timestamp_example.json'");
 //	auto result = con.Query("select * from read_json_objects('/home/liyu/duckdb/data/json/timestamp_example.json')");
 //	auto result = con.Query("select fuck('Jane') as result");
-	auto result = con.Query("select * from '/home/liyu/pixels-reader-cxx/tests/data/20230316154717_0.pxl'");
+	auto result = con.Query("select sum(n_nationkey) from '/home/liyu/pixels-reader-cxx/tests/data/20230316154717_0.pxl'");
 	result->Print();
 }
