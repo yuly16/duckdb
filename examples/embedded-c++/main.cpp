2e518c50807659dbb925f38fc619e6decb4e125f
yuliangyong <yuliangyong8295019@gmail.com>
2023-04-07 10:20:29 +0200

    support date

--- examples/embedded-c++/main.cpp
+++ examples/embedded-c++/main.cpp
@@ -10,13 +10,26 @@ int main() {
 //	con.Query("INSERT INTO integers VALUES (3)");
 //	auto result = con.Query("SELECT * FROM integers");
 //	auto result = con.Query("select count(*) from '/home/liyu/duckdb/data/csv/customer.csv'");
-//	auto result = con.Query("select name from '/home/liyu/duckdb/data/parquet-testing/candidate.parquet' limit 2");
-//	auto result = con.Query("select l3 from '/home/liyu/duckdb/data/parquet-testing/decimals.parquet'");
-//	auto result = con.Query("select * from '/home/liyu/duckdb/data/json/timestamp_example.json'");
+//	auto result = con.Query("select * from '/home/yuly/project/duckdb/data/parquet-testing/date.parquet'");
+
+	// large table parquet example
+//	{
+//		auto result = con.Query("select * from '/home/yuly/project/duckdb/data/parquet-testing/leftdate3_192_loop_1.parquet'");
+//		result->Print();
+//	}
+
+	//	auto result = con.Query("select * from '/home/liyu/duckdb/data/json/timestamp_example.json'");
 //	auto result = con.Query("select * from read_json_objects('/home/liyu/duckdb/data/json/timestamp_example.json')");
 //	auto result = con.Query("select fuck('Jane') as result");
 //	auto result = con.Query("select n_name from '/home/yuly/project/pixels-reader-cxx/tests/data/nation_0_1.pxl'");
-//	result->Print();
-	auto result1 = con.Query("select * from '/home/yuly/project/pixels-reader-cxx/tests/data/supplier_0_1.pxl'");
-	result1->Print();
+
+//	auto result1 = con.Query("select * from '/home/yuly/project/pixels-reader-cxx/tests/data/supplier_0_1.pxl'");
+//	result1->Print();
+
+	// pixels
+	{
+		auto result = con.Query("select * from '/home/yuly/project/data/orders_0_1.pxl' limit 100");
+		result->Print();
+	}
+
 }
