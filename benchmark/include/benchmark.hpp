bfeadb698f48ba55cfc6879f3d03427eb4ccd3f4
Liangyong Yu <yuliangyong8295019@gmail.com>
2023-04-16 18:02:33 +0200

    update timeout value in benchmark; add tpch 300 benchmark evaluation

--- benchmark/include/benchmark.hpp
+++ benchmark/include/benchmark.hpp
@@ -29,7 +29,7 @@ struct BenchmarkState {
 //! new benchmarks
 class Benchmark {
 	constexpr static size_t DEFAULT_NRUNS = 5;
-	constexpr static size_t DEFAULT_TIMEOUT = 30;
+	constexpr static size_t DEFAULT_TIMEOUT = 30000;
 
 	Benchmark(Benchmark &) = delete;
 
