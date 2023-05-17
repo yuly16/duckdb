917938ba405b55b5bb579743d45ccd96bd10c6a3
Liangyong Yu <yuliangyong8295019@gmail.com>
2023-05-17 15:57:04 +0200

    create encoding and small endian benchmark

--- scripts/pixels_generate_tpch_benchmark.py
+++ scripts/pixels_generate_tpch_benchmark.py
@@ -1,8 +1,8 @@
 from os import path, mkdir
-sf = 300
+sf = 1
 root_path = "benchmark/tpch/pixels"
-enable_verification = False
-sf_path = path.join(root_path, "tpch_" + str(sf))
+enable_verification = True
+sf_path = path.join(root_path, "tpch_" + str(sf) + "_small_endian")
 if not path.exists(sf_path):
     mkdir(sf_path)
 for i in range(1, 23):
@@ -11,9 +11,9 @@ for i in range(1, 23):
     else:
         f = open(path.join(sf_path, "pixels_q" + str(i) + "_tpch_" + str(sf) + ".benchmark"), "w")
     if enable_verification:
-        f.writelines("template benchmark/tpch/pixels/pixels_tpch_template.benchmark.in\n")
+        f.writelines("template benchmark/tpch/pixels/pixels_small_endian_tpch_template.benchmark.in\n")
     else:
-        f.writelines("template benchmark/tpch/pixels/pixels_tpch_template_no_verification.benchmark.in\n")
+        f.writelines("template benchmark/tpch/pixels/pixels_small_endian_tpch_template_no_verification.benchmark.in\n")
     f.writelines("SF_NUMBER=" + str(sf) + "\n")
     f.writelines("SF_NUMBER_UNDERSCORE=" + str(sf) + "\n")
     if i < 10:
