f9b072383830a7c9c0bc8e3927dc9ea82ae13006
Liangyong Yu <yuliangyong8295019@gmail.com>
2023-08-10 01:49:58 +0200

    add a split multi dir python script

--- src/common/multi_file_reader.cpp
+++ src/common/multi_file_reader.cpp
@@ -34,10 +34,8 @@ vector<string> MultiFileReader::GetFileList(ClientContext &context, const Value 
 			if (val.IsNull()) {
 				throw ParserException("%s reader cannot take NULL input as parameter", name);
 			}
-            if(fs.DirectoryExists(StringValue::Get(val))) {
-                auto glob_files = fs.GlobFiles(StringValue::Get(val), context, options);
-                files.insert(files.end(), glob_files.begin(), glob_files.end());
-            }
+            auto glob_files = fs.GlobFiles(StringValue::Get(val), context, options);
+            files.insert(files.end(), glob_files.begin(), glob_files.end());
 		}
 	} else {
 		throw InternalException("Unsupported type for MultiFileReader::GetFileList");
