66a5f314e15999f037ef285cbe5d3d3364b61425
yuliangyong <yuliangyong8295019@gmail.com>
2024-06-02 15:32:12 +0000

    change main function to read pixels-example in PIXELS_SRC

--- examples/pixels-example/main.cpp
+++ examples/pixels-example/main.cpp
@@ -7,7 +7,7 @@ using namespace duckdb;
 int main() {
 	DuckDB db(nullptr);
 	Connection con(db);
-	std::string demo = ConfigFactory::Instance().getPixelsDirectory() + "cpp/tests/data/example.pxl";
+	std::string demo = ConfigFactory::Instance().getPixelsSourceDirectory() + "cpp/tests/data/example.pxl";
 	auto result = con.Query("SELECT * from '" + demo + "';");
 	result->Print();
 }
